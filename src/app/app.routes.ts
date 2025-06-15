5325ca31581d47f97537be97181c1cb42f4e7d55
Bernard Uriza Orozco <bernarduriza@gmail.com>
2025-06-14 21:55:48 -0600

    Siguiente: Skills feature (en construcción)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { provideRouter, Routes } from '@angular/router';
 import { ProjectList } from './features/projects/project-list/project-list';
+import { SkillList } from './features/skills/skill-list/skill-list';
 
 export const routes: Routes = [
   { path: 'projects', component: ProjectList },
+  { path: 'skills', component: SkillList },
   { path: '', redirectTo: 'projects', pathMatch: 'full' }
 ];
 
