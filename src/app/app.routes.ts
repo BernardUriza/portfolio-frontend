0ffa47db69a9da6fbd037efd64c99ceb5e9d6725
Bernard Uriza Orozco <bernarduriza@gmail.com>
2025-06-14 21:40:40 -0600

    router fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
+import { provideRouter, Routes } from '@angular/router';
 import { ProjectList } from './features/projects/project-list/project-list';
 
-export const routes = [
+export const routes: Routes = [
   { path: 'projects', component: ProjectList },
   { path: '', redirectTo: 'projects', pathMatch: 'full' }
+];
+
+export const appConfig = [
+  provideRouter(routes)
 ];
