546013b7d65b262a723dbc88a4575f6ccee57f08
Bernard Uriza Orozco <bernarduriza@gmail.com>
2025-06-15 16:47:46 -0600

    tailwind

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header';
+import { HeroComponent } from './components/hero/hero';
+import { CarouselComponent } from './components/carousel/carousel';
+import { AboutComponent } from './components/about/about';
+import { CasesComponent } from './components/cases/cases';
+import { ServicesComponent } from './components/services/services';
+import { ContactComponent } from './components/contact/contact';
+import { FooterComponent } from './components/footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    CarouselComponent,
+    AboutComponent,
+    CasesComponent,
+    ServicesComponent,
+    ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
   standalone: true
