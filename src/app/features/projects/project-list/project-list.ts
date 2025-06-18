a9a15a69b675c20f6212b153b9a3ebf67adf0040
Bernard Uriza Orozco <bernarduriza@gmail.com>
2025-06-17 18:47:29 -0600

    fix(project-list): add track expression to @for loop for Angular 17 compliance

--- src/app/features/projects/project-list/project-list.ts
+++ src/app/features/projects/project-list/project-list.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService, Project } from '../project.service';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-project-list',
   standalone: true,
   templateUrl: './project-list.html',
   styleUrls: ['./project-list.scss'],
-  imports: [] // agrega lo necesario (NgForOf, CommonModule, etc.)
+  imports: [CommonModule] 
 })
-export class ProjectListComponent implements OnInit {
+export class ProjectList implements OnInit {
   projects: Project[] = [];
 
   constructor(private projectService: ProjectService) {}
