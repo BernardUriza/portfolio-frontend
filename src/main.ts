19883e7930824fc211d97cae211deb69b9210b78
Bernard Uriza Orozco <bernarduriza@gmail.com>
2025-06-20 23:09:42 -0600

    fix(i18n): move TranslateLoader provider to bootstrap, cleanup TranslateModule imports
    Avoids invalid use of ModuleWithProviders in standalone components.

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { App } from './app/app';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { appConfig } from './app/app.config';
 import { provideHttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpLoaderFactory } from './app/app'; // o la ruta donde exportes tu factory
+import { HttpClient } from '@angular/common/http';
 
 bootstrapApplication(App, {
   ...appConfig,
   providers: [
     provideHttpClient(),
     provideAnimations(),
+    {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [HttpClient]
+    },
+    TranslateModule
+    ,
     ...(appConfig.providers || [])
   ]
 }).catch(err => console.error(err));
