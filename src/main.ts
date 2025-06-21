e146e106918f5ab4e3409b33e138b3c176400925
Bernard Uriza Orozco <bernarduriza@gmail.com>
2025-06-20 23:59:33 -0600

    ft

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, HttpClient } from '@angular/common/http';
-import { TranslateLoader } from '@ngx-translate/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateService, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
-export const PROVIDE_TRANSLATE = [
-  {
-    provide: TranslateLoader,
-    useFactory: HttpLoaderFactory,
-    deps: [HttpClient]
-  }
-];
 
 bootstrapApplication(App, {
   providers: [
     provideHttpClient(),
     provideAnimations(),
-  ...PROVIDE_TRANSLATE
+    {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [HttpClient]
+    }
   ]
+}).then(appRef => {
+  const translate = appRef.injector.get(TranslateService);
+  translate.addLangs(['en', 'es']);
+  translate.setDefaultLang('en');
+  const browserLang = translate.getBrowserLang() || 'en';
+  translate.use(browserLang.match(/en|es/) ? browserLang : 'en');
 });
